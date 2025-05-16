52c5dfeb8cc9a3cd54243968408696f24ef2f732
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-05-16 20:16:01 +0300

    add base echo bot example

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 
 	"github.com/cmczk/rssources-bot/pkg/config"
+	"github.com/mymmrac/telego"
+	tu "github.com/mymmrac/telego/telegoutil"
 )
 
 func main() {
 	t := config.MustToken()
-	su := config.MustStorageURL()
+	_ = config.MustStorageURL()
+	ctx := context.Background()
 
-	fmt.Println(t)
-	fmt.Println(su)
+	bot, err := telego.NewBot(t, telego.WithDefaultDebugLogger())
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	updates, err := bot.UpdatesViaLongPolling(ctx, nil)
+	if err != nil {
+		fmt.Printf("error while long polling: %s", err.Error())
+	}
+
+	for u := range updates {
+		fmt.Printf("%+v\n", u)
+
+		if u.Message != nil {
+			chatID := u.Message.Chat.ID
+
+			_, _ = bot.SendMessage(
+				ctx,
+				tu.Message(
+					tu.ID(chatID),
+					u.Message.Text,
+				),
+			)
+		}
+	}
 }
