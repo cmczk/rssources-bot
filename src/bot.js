c8a3e2d8b334b903638eac28ed3323c57f7bd650
Yauheni Kuntsevich <cmaczok@gmail.com>
2025-06-19 02:25:11 +0300

    initial commit

---
+++ src/bot.js
@@ -0,0 +1,10 @@
+import { Bot } from "grammy";
+import "dotenv/config";
+
+const bot = new Bot(process.env.BOT_API_KEY);
+
+bot.command("start", (ctx) => {
+  ctx.reply("Hello, world!");
+});
+
+bot.start();
